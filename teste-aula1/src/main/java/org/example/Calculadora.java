4c5ee25cd6d43feaefc35b7032a1691f9f96cca2
André Nunes <andre.n@edu.sc.senai.br>
2023-05-29 21:22:26 -0300

    aula1

---
+++ teste-aula1/src/main/java/org/example/Calculadora.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Calculadora {
+
+    public Double soma(Double valor1, Double valor2){
+        return valor1+valor2+10.0;
+    }
+}
---
+++ teste-aula1/src/test/java/org/example/CalculadoraTeste.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTeste {
+
+    @Test // indica que um método é um método de teste
+    // o método somaTeste realiza o teste do método soma de Calculadora
+    // é o teste unitário do método soma
+    void somaTeste() {
+        // criar um objeto calculadora
+        Calculadora calculadora = new Calculadora();
+
+        // realiza a chamada do soma com valores pré definidos
+        double resultado = calculadora.soma(1.0,2.2);
+
+        // valida o resultado da soma
+        // o assertEquals valida se o valor do 1º parametro é igual ao valor do 2º
+        assertEquals(13.2,resultado);
+        assertNotEquals(0,resultado);
+    }
+}
